2e59d510b4715ec0adc53ce01324aab6273a865c
Jameslu <a905538@gmail.com>
2023-08-24 16:21:17 +0800

    upload text ok

--- text-import.php
+++ text-import.php
@@ -1,4 +1,5 @@
 <?php
+
 /****
  * 1.建立資料庫及資料表
  * 2.建立上傳檔案機制
@@ -9,25 +10,88 @@
  * 7.結束檔案
  */
 
+
+if (!empty($_FILES['text']['tmp_name'])) {
+    echo "檔名是:" . $_FILES['text']['name'];
+    echo "<br>";
+    echo "檔案大小是:" . $_FILES['text']['size'];
+    echo "<hr>";
+
+    move_uploaded_file($_FILES['text']['tmp_name'], "./doc/{$_FILES['text']['name']}");
+    $path = "./doc/{$_FILES['text']['name']}";
+    // 使用 'r' 模式來讀取檔案
+    $file = fopen($path, "r");
+    $dataTR = "";
+    // echo "<table>";
+    if ($file) {
+        // 讀取檔案到最後
+        while (($line = fgets($file)) !== false) {
+            $dataTR .= "<tr>";
+            // echo $line;
+            // $data = str_getcsv($line);
+            $cols = explode(",", $line);
+            // dd($data);
+            for ($i = 0; $i < count($cols); $i++) {
+                $dataTR .= "<td>" . $cols[$i] . "</td>";
+            }
+            $dataTR .= "</tr>";
+            // echo "<br>";
+        }
+        fclose($file);
+    } else {
+        // 檔案開啟失敗
+        echo "檔案開啟失敗";
+    }
+    // echo "</table>";
+}
+function dd($data)
+{
+    echo "<pre>";
+    print_r($data);
+    echo "</pre>";
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>文字檔案匯入</title>
     <link rel="stylesheet" href="style.css">
+    <style>
+        table {
+            border: 1px solid #ccc;
+            box-shadow: 3px 3px 15px #aaa;
+            padding: 20px;
+            border-collapse: collapse;
+        }
+
+        td {
+            border: 1px solid #ccc;
+            padding: 5px 10px;
+            text-align: center;
+        }
+    </style>
 </head>
+
 <body>
-<h1 class="header">文字檔案匯入練習</h1>
-<!---建立檔案上傳機制--->
+    <h1 class="header">文字檔案匯入練習</h1>
+    <!---建立檔案上傳機制--->
+    <form action="?" method="post" enctype="multipart/form-data">
+        <input type="file" name="text" id="text">
+        <input type="submit" value="上傳">
+        <table>
+            <?= $dataTR; ?>
+        </table>
+    </form>
 
 
-
-<!----讀出匯入完成的資料----->
+    <!----讀出匯入完成的資料----->
 
 
 
 </body>
+
 </html>
